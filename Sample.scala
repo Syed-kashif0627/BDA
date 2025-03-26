615063140185d923cd0bbe77e0def4c28308ccf6
Syed Abdul Quadeer Kashif <113990051+Syed-kashif0627@users.noreply.github.com>
2025-03-26 11:16:17 +0530

    Scala Basics

---
+++ Sample.scala
@@ -0,0 +1,17 @@
+object Sample extends App{
+    println("Data Science MTech");
+    println("Wednesday");
+    var x=0;
+    x=5
+    val z=x+5//It is like const in java cant be changed once declared
+    println(x)
+    println(z)
+    var y:String="";
+    println("Enter Your Name:")
+    //Input of String in scala
+    y=scala.io.StdIn.readLine;
+    //Input of an Int in scala
+    println("Enter your age:")
+    val num=scala.io.StdIn.readLine.toInt;
+    println("Name is "+y+" Age is "+num)
+}
